a932fdc151e4faa26a8a6ed845e01fcf289e9411
wangwen <ww@gmail.com>
2022-01-20 14:51:40 +0800

    init code

---
+++ js/awards.js
@@ -0,0 +1,79 @@
+//开奖列表
+window.awards = [
+    {
+        remark: "第一轮 詹总",
+        name: '三等奖',
+        award: '200元',
+        count: 10,
+    },
+    {
+        remark: "第一轮 詹总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    },
+    //丁总
+    {
+        remark: "第一轮 丁总",
+        name: '二等奖',
+        award: '400元',
+        count: 6,
+    },
+    {
+        remark: "第一轮 丁总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    },
+    //杜总
+    {
+        remark: "第一轮 杜总",
+        name: '一等奖',
+        award: '600元',
+        count: 4,
+    },
+    {
+        remark: "第一轮 杜总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    },
+    // 第二轮
+    {
+        remark: "第二轮 詹总",
+        name: '三等奖',
+        award: '200元',
+        count: 10,
+    },
+    {
+        remark: "第二轮 詹总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    },
+    //丁总
+    {
+        remark: "第二轮 丁总",
+        name: '二等奖',
+        award: '400元',
+        count: 6,
+    },
+    {
+        remark: "第二轮 丁总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    },
+    //杜总
+    {
+        remark: "第二轮 杜总",
+        name: '一等奖',
+        award: '600元',
+        count: 4,
+    },
+    {
+        remark: "第二轮 杜总",
+        name: '幸运奖',
+        award: '100元',
+        count: 6,
+    }];
---
+++ js/members.js
@@ -0,0 +1,2 @@
+//全体人员列表，请使用Excel导入
+window.members = [];
